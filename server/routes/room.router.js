e1fb861f30a594fb9509409ec7c1f775cafdf02a
Zeke Taves <ezekielprof@gmail.com>
2023-06-19 15:46:54 -0500

    Room posting is functional

--- server/routes/room.router.js
+++ server/routes/room.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 // START GET
 
+// Getting list of all rooms
 router.get('/', async (req, res) => {
     try {
         // Convinient variable for longish query
@@ -17,8 +18,28 @@ router.get('/', async (req, res) => {
         res.sendStatus(500);
         console.log('Getting rooms error!', error);
     }
-})
+});
 
 // END GET
+
+// START POST
+
+router.post('/', async (req, res) => {  // Expecting {room_name, type, creator_id}
+    try {
+        // Convenient pool query variable
+        const query =
+            'INSERT INTO "chatroom" ("room_name", "type", "creator_id") VALUES ' +
+            '($1, \'public\', $2);'
+        const response = await pool.query(query, [req.body.room_name, req.body.creator_id]);
+        
+        // Tell client it's all good
+        res.sendStatus(201);
+    } catch (error) {
+        console.log('Nope, room posting error!', error);
+        res.sendStatus(500);
+    }
+});
+
+// END POST
 
 module.exports = router;
