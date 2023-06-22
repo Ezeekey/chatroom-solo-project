32e7e397890710445c5214d84fccfce2c3d23d05
Zeke Taves <ezekielprof@gmail.com>
2023-06-22 09:43:19 -0500

    Cleaned up room router

--- server/routes/room.router.js
+++ server/routes/room.router.js
@@ -2,15 +2,17 @@ const express = require('express');
 const pool = require('../modules/pool.js');
 const router = express.Router();
 
+const { rejectUnauthenticated } = require('../modules/authentication-middleware.js');
+
 // START GET
 
 // Getting list of all rooms
-router.get('/', async (req, res) => {
+router.get('/', rejectUnauthenticated, async (req, res) => {
     try {
         // Convenient variable for longish query
         const query =
             'SELECT "chatroom"."id", "room_name", "username", "type" FROM "chatroom" ' +
-            'JOIN "user" ON "user"."id" = "creator_id";'
+            'JOIN "user" ON "user"."id" = "creator_id" WHERE type=\'public\';'
         const response = await pool.query(query);
         // Send to client
         res.send(response.rows);
@@ -21,7 +23,7 @@ router.get('/', async (req, res) => {
 });
 
 // Getting the name of a room, I don't like
-router.get('/:id', async (req, res) => {
+router.get('/:id', rejectUnauthenticated, async (req, res) => {
     try {
         // Getting name from the database using id
         const response = await pool.query('SELECT room_name FROM chatroom WHERE id = $1', [req.params.id]);
@@ -36,13 +38,13 @@ router.get('/:id', async (req, res) => {
 
 // START POST
 
-router.post('/', async (req, res) => {  // Expecting {room_name, type, creator_id}
+router.post('/', rejectUnauthenticated, async (req, res) => {  // Expecting {room_name, type}
     try {
         // Convenient pool query variable
         const query =
             'INSERT INTO "chatroom" ("room_name", "type", "creator_id") VALUES ' +
-            '($1, \'public\', $2);'
-        const response = await pool.query(query, [req.body.room_name, req.body.creator_id]);
+            '($1, $2, $3);'
+        const response = await pool.query(query, [req.body.room_name, req.body.type, req.user.id]);
 
         // Tell client it's all good
         res.sendStatus(201);
@@ -56,12 +58,12 @@ router.post('/', async (req, res) => {  // Expecting {room_name, type, creator_i
 
 // START DELETE
 
-router.delete('/:id', async (req, res) => {    // Expecting user id in query params
+router.delete('/:id', rejectUnauthenticated, async (req, res) => {    // id param is room_id
     try {
         // Convenient query text variable
         const query =
             'DELETE FROM "chatroom" WHERE "creator_id" = $1 AND "id" = $2;';
-        pool.query(query, [req.query.creator_id, req.params.id]);
+        pool.query(query, [req.user.id, req.params.id]);
 
         // Send success to client
         res.sendStatus(204);
